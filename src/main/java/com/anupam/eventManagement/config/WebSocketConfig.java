e514bed7523d61c043a982f9cd534ec820869d7d
Anupam Tarai <anupam@esspl.com>
2025-11-27 14:37:14 +0530

    Update config

--- src/main/java/com/anupam/eventManagement/config/WebSocketConfig.java
+++ src/main/java/com/anupam/eventManagement/config/WebSocketConfig.java
@@ -17,6 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173").withSockJS(); // Register STOMP endpoint
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5173","https://event-phi-one.vercel.app").withSockJS(); // Register STOMP endpoint
     }
 }
